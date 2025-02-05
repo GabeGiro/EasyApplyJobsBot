6f9360437801e86c7d04c0c847d7d642dd7c0f67
Gabe Giro <gabriel.gircenko@gmail.com>
2025-02-06 00:17:31 +0100

    One line formatted

--- tests/test_question_handling.py
+++ tests/test_question_handling.py
@@ -53,7 +53,7 @@ class test_getting_job_details_from_linkedin_job_post(BaseTestCase):
                 cls.processor.driverHelper.clickReviewApplicationButton()
 
             if cls.processor.driverHelper.isQuestionsUnansweredErrorMessageDisplayed():
-                    return job
+                return job
 
 
     def setUp(self):
